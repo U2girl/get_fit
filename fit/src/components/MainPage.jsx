49e7a0ebf02fafe3b78e8db3eb7a734f764dec78
U2girl <elycias@yahoo.com>
2023-11-28 15:31:50 -0500

    header

--- fit/src/components/MainPage.jsx
+++ fit/src/components/MainPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import HeaderPage from './HeaderPage'
+// import HeroPage from './HeroPage'
 
 const MainPage = () => {
   return (
     <React.Fragment>
-    <section>
-      <div>
-        <p> hello mainPage</p>
+    <main>
+      <div className="bg-white pb-20 shadow-lg md:w-2/3 md:mx-auto">
+        <HeaderPage/>
+        {/* <HeroPage/> */}
       </div>
-    </section>
+    </main>
   </React.Fragment>
   )
 }
